f59b11f03486c1f87661eb632636dec745e4519b
jestruvec <jestruve@thepalacecompany.com>
2026-06-25 17:20:43 -0600

    feat: enhance localization

--- src/i18n/utilities.ts
+++ src/i18n/utilities.ts
@@ -1,5 +1,10 @@
 import { defaultLang, ui } from "./ui";
 
+export function getLangFromUrl(url: URL): "en" | "es" {
+  const [, lang] = url.pathname.split("/");
+  return lang === "en" || lang === "es" ? lang : defaultLang;
+}
+
 export function useTranslations(lang: keyof typeof ui) {
   return function t(key: keyof (typeof ui)[typeof defaultLang]) {
     return ui[lang][key];
